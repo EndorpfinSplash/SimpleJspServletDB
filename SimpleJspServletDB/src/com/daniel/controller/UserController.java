1238f2ae3507f60bf8eaee802725ecf2cdac8a89
AZ <fast.optimus@gmail.com>
2019-06-17 10:50:30 +0300

    Reformatted

--- SimpleJspServletDB/src/com/daniel/controller/UserController.java
+++ SimpleJspServletDB/src/com/daniel/controller/UserController.java
@@ -1,18 +1,17 @@
 package com.daniel.controller;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.daniel.dao.UserDao;
+import com.daniel.model.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.daniel.dao.UserDao;
-import com.daniel.model.User;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UserController extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -26,20 +25,20 @@ public class UserController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String forward="";
+        String forward = "";
         String action = request.getParameter("action");
 
-        if (action.equalsIgnoreCase("delete")){
+        if (action.equalsIgnoreCase("delete")) {
             int userId = Integer.parseInt(request.getParameter("userId"));
             dao.deleteUser(userId);
             forward = LIST_USER;
-            request.setAttribute("users", dao.getAllUsers());    
-        } else if (action.equalsIgnoreCase("edit")){
+            request.setAttribute("users", dao.getAllUsers());
+        } else if (action.equalsIgnoreCase("edit")) {
             forward = INSERT_OR_EDIT;
             int userId = Integer.parseInt(request.getParameter("userId"));
             User user = dao.getUserById(userId);
             request.setAttribute("user", user);
-        } else if (action.equalsIgnoreCase("listUser")){
+        } else if (action.equalsIgnoreCase("listUser")) {
             forward = LIST_USER;
             request.setAttribute("users", dao.getAllUsers());
         } else {
@@ -62,12 +61,9 @@ public class UserController extends HttpServlet {
         }
         user.setEmail(request.getParameter("email"));
         String userid = request.getParameter("userid");
-        if(userid == null || userid.isEmpty())
-        {
+        if (userid == null || userid.isEmpty()) {
             dao.addUser(user);
-        }
-        else
-        {
+        } else {
             user.setUserid(Integer.parseInt(userid));
             dao.updateUser(user);
         }
